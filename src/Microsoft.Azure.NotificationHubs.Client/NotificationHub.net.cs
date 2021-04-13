02a1a55e0cb03a7de41d3a9af339847db05bbae3
Muhammad Miftah <mamift@gmail.com>
2021-04-13 14:39:05 +1000

    Added an exception that details what method is not implemented on .net standard.

--- src/Microsoft.Azure.NotificationHubs.Client/NotificationHub.net.cs
+++ src/Microsoft.Azure.NotificationHubs.Client/NotificationHub.net.cs
@@ -4,31 +4,33 @@ namespace Microsoft.Azure.NotificationHubs.Client
 {
     public partial class NotificationHub
     {
-        static string PlatformPushChannel => throw new NotImplementedException();
-        static void PlatformSaveInstallation() => throw new NotImplementedException();
-        static void PlatformInitialize(string connectionString, string hubName) => throw new NotImplementedException();
-        static void PlatformInitialize(IInstallationManagementAdapter installationManagementAdapter) => throw new NotImplementedException();
-        static void PlatformSetEnricher() => throw new NotImplementedException();
+        private const string NotImplementedExceptionMessage = "This method is not implemented on .NET Standard platforms. ";
 
-        static string PlatformGetUserId() => throw new NotImplementedException();
-        static void PlatformSetUserId(string value) => throw new NotImplementedException();
+        static string PlatformPushChannel => throw new NotImplementedException(NotImplementedExceptionMessage + nameof(PlatformPushChannel));
+        static void PlatformSaveInstallation() => throw new NotImplementedException(NotImplementedExceptionMessage + nameof(PlatformSaveInstallation) + "()");
+        static void PlatformInitialize(string connectionString, string hubName) => throw new NotImplementedException(NotImplementedExceptionMessage + nameof(PlatformInitialize) + "()");
+        static void PlatformInitialize(IInstallationManagementAdapter installationManagementAdapter) => throw new NotImplementedException(NotImplementedExceptionMessage + nameof(PlatformInitialize) + "()");
+        static void PlatformSetEnricher() => throw new NotImplementedException(NotImplementedExceptionMessage + nameof(PlatformSetEnricher) + "()");
+
+        static string PlatformGetUserId() => throw new NotImplementedException(NotImplementedExceptionMessage + nameof(PlatformGetUserId) + "()");
+        static void PlatformSetUserId(string value) => throw new NotImplementedException(NotImplementedExceptionMessage + nameof(PlatformSetUserId) + "()");
 
         #region Tags
 
-        static bool PlatformAddTag(string tag) => default;
-        static bool PlatformAddTags(string[] tags) => default;
-        static void PlatformClearTags() => throw new NotImplementedException();
-        static bool PlatformRemoveTag(string tag) => default;
-        static bool PlatformRemoveTags(string[] tags) => default;
-        static string[] PlatformGetTags() => default;
+        static bool PlatformAddTag(string tag) => throw new NotImplementedException(NotImplementedExceptionMessage + nameof(PlatformAddTag) + "()");
+        static bool PlatformAddTags(string[] tags) => throw new NotImplementedException(NotImplementedExceptionMessage + nameof(PlatformAddTags) + "()");
+        static void PlatformClearTags() => throw new NotImplementedException(NotImplementedExceptionMessage + nameof(PlatformClearTags) + "()");
+        static bool PlatformRemoveTag(string tag) => throw new NotImplementedException(NotImplementedExceptionMessage + nameof(PlatformRemoveTag) + "()");
+        static bool PlatformRemoveTags(string[] tags) => throw new NotImplementedException(NotImplementedExceptionMessage + nameof(PlatformRemoveTags) + "()");
+        static string[] PlatformGetTags() => throw new NotImplementedException(NotImplementedExceptionMessage + nameof(PlatformGetTags) + "()");
 
         #endregion
 
         #region Templates
 
-        static void PlatformSetTemplate(string name, InstallationTemplate template) => throw new NotImplementedException();
-        static void PlatformRemoveTemplate(string name) => throw new NotImplementedException();
-        static InstallationTemplate PlatformGetTemplate(string name) => throw new NotImplementedException();
+        static void PlatformSetTemplate(string name, InstallationTemplate template) => throw new NotImplementedException(NotImplementedExceptionMessage + nameof(PlatformSetTemplate) + "()");
+        static void PlatformRemoveTemplate(string name) => throw new NotImplementedException(NotImplementedExceptionMessage + nameof(PlatformRemoveTemplate) + "()");
+        static InstallationTemplate PlatformGetTemplate(string name) => throw new NotImplementedException(NotImplementedExceptionMessage + nameof(PlatformGetTemplate) + "()");
 
         #endregion
     }
